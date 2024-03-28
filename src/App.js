ce82ef6d17d2e8e5d61ba42bde5883ff1c5b41e3
AymenNF <aymenneffar111@gmail.com>
2024-03-28 15:24:19 +0000

    c

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter , Route, Routes, Navigate } from 'react-router-dom';
+import './App.css';
+import NavbarCard from './componenets/NavbarCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SideBar from './componenets/SideBar';
+import Main from './componenets/Main';
+import Messages from './componenets/Messages';
+import Login from './componenets/Login';
+import Signin from './componenets/Signin';
+import MessagesDetails from './componenets/MessagesDetails';
+import withAuth from './componenets/WithAuth';
+import ChatPage from './componenets/ChatPage';
+import RoomPage from './componenets/RoomPage';
+import StreamPage from './componenets/StreamPage';
+
+function App() {
+
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+
+    <div>
+
+        <div className="">
+           {/* <Messages></Messages> */}
+          {/* <Login></Login> */}
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={withAuth(Messages)} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/Login" element={<Login />} />
+              <Route path="/chat" element={<ChatPage />} />
+              <Route path="/room" element={<StreamPage />} />
+              <Route path="/room/:roomID" element={<RoomPage />} />
+              <Route path="*" element={withAuth(Messages)} />
+              {localStorage.getItem('token') && <Route path="*" element={<Navigate to="/" />} />}
+            </Routes>
+          </BrowserRouter>
+        </div>
+       
+
+    </div>
+
+  );
+}
+
+export default App;
